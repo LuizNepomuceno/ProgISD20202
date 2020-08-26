d6f03404bc9bc1023bc50fdf80175be0052b4e01
LuizNepomuceno <69309587+LuizNepomuceno@users.noreply.github.com>
2020-08-26 11:09:58 -0300

    Todas as Atividades da Aula 3
    Todos os Arquivos são referentes a aula 3.
    A segunda parte do IMC pertence a aula 4

--- Atividade1.py
+++ Atividade1.py
@@ -1,6 +1,5 @@
-# Aula_3_IMC
-Atividade da aula 3 para criar um código que possa calcular o IMC
-Índice de massa corporal (IMC):
+#Índice de massa corporal (IMC):
+'''
 Requisitos:
 𝑖𝑚𝑐=𝑝𝑒𝑠𝑜/(𝑎𝑙𝑡𝑢𝑟𝑎^2 )
 Muito abaixo do peso  quando imc menor que 17
@@ -10,6 +9,19 @@ Acima do peso normal  quando imc entre 25 e 30
 Muito acima do peso quando imc acima de 30
 Somente mostrar true e false (sem estrutura de decisão)
 Criar e gerenciar projeto:
+Trabalhar em grupos com 4 no projeto, com todos fazendo o programa todo dessa vez (fluxo de trabalho conforme aula anterior (Fluxo de trabalho do Gerente de Integração)). Selecionar o gerente de projeto e o gerente de integração na equipe.
 Issues
 Milestones
 Documentar (wiki)
+'''
+
+#Atividade 3 IMC
+
+
+
+peso = 73
+altura = 1.70**2
+print(altura)
+
+print(peso / altura)
+
